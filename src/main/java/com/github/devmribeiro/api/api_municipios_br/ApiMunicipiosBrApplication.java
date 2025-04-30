64dad26a8d4ade24861c0eb0a45b08e3f3a37b72
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-29 23:55:26 -0300

    First commit

---
+++ src/main/java/com/github/devmribeiro/api/api_municipios_br/ApiMunicipiosBrApplication.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.api.api_municipios_br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMunicipiosBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMunicipiosBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/devmribeiro/api/api_municipios_br/ApiMunicipiosBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.api.api_municipios_br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMunicipiosBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
