e238bbe5abd972ae067d730a11d7e74a91e7b579
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-30 23:57:07 -0300

    refactor: remove empty lines

---
+++ src/main/java/com/github/devmribeiro/api/municipios/app/ApiMunicipiosBrApplication.java
@@ -0,0 +1,11 @@
+package com.github.devmribeiro.api.municipios.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMunicipiosBrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMunicipiosBrApplication.class, args);
+	}
+}
