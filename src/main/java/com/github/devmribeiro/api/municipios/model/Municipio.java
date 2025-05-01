e94a11677e7e648b8a6ef526f4f494ba199c6c16
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-30 23:56:19 -0300

    feat: create Municipio entity

---
+++ src/main/java/com/github/devmribeiro/api/municipios/model/Municipio.java
@@ -0,0 +1,40 @@
+package com.github.devmribeiro.api.municipios.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "municipio")
+public class Municipio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "municipio_id")
+	private Long municipioId = null;
+
+	private String nome = null;
+
+	private String uf = null;
+
+	public Municipio(Long municipioId, String nome, String uf) {
+		this.municipioId = municipioId;
+		this.nome = nome;
+		this.uf = uf;
+	}
+
+	public Long getMunicipioId() {
+		return municipioId;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+}
