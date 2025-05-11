6be11da0d356dc3b3bd97f85bbcaeb16eb629d57
devMRibeiro <michael.douglas2020@hotmail.com>
2025-05-11 05:48:26 -0300

    refactor: cria query customizado para buscar municipios por uf

--- src/main/java/com/github/devmribeiro/api/municipios/repository/MunicipioRepository.java
+++ src/main/java/com/github/devmribeiro/api/municipios/repository/MunicipioRepository.java
@@ -3,6 +3,8 @@ package com.github.devmribeiro.api.municipios.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.github.devmribeiro.api.municipios.model.Municipio;
@@ -10,5 +12,7 @@ import com.github.devmribeiro.api.municipios.model.Municipio;
 @Repository
 public interface MunicipioRepository extends JpaRepository<Municipio, Long> {
 	Municipio findByNome(String nome);
-	List<Municipio> findByUf(String uf);
+
+	@Query(value = "SELECT m.* FROM municipio m JOIN unidade_federativa uf ON m.mun_uf_cod = uf.uf_cod WHERE uf.uf_sigla = :sigla", nativeQuery = true)
+	List<Municipio> findByUfSigla(@Param("sigla") String sigla);
 }
