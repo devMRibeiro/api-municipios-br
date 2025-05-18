295460fd24e2d4e7ffb67318e549cb9c2d5bf67d
devMRibeiro <michael.douglas2020@hotmail.com>
2025-05-18 10:48:59 -0300

    minor changes

--- src/main/java/com/github/devmribeiro/api/municipios/controller/MunicipioController.java
+++ src/main/java/com/github/devmribeiro/api/municipios/controller/MunicipioController.java
@@ -1,6 +1,8 @@
 package com.github.devmribeiro.api.municipios.controller;
 
-import org.apache.coyote.BadRequestException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.devmribeiro.api.municipios.dto.MunicipioDTO;
+import com.github.devmribeiro.api.municipios.model.Municipio;
 import com.github.devmribeiro.api.municipios.repository.MunicipioRepository;
 
 @RestController
@@ -21,21 +24,31 @@ public class MunicipioController {
 	}
 
 	@GetMapping
-	public ResponseEntity<?> get(@RequestParam(name = "uf", required = false) String uf) {
-	    if (uf == null)
-	        return ResponseEntity.ok(repository
-	        		.findAll()
-	        		.stream()
-	        		.map(municipio -> new MunicipioDTO(municipio))
-	        		.toList());
+	public ResponseEntity<?> get(
+	        @RequestParam(name = "uf", required = false) String uf,
+	        @RequestParam(name = "mun", required = false) String municipioNome) {
 
-	    if (uf.trim().isEmpty() || uf.length() > 2 || uf.replaceAll("[\"']", "").trim().isEmpty())
-	        return ResponseEntity.badRequest().body(new BadRequestException("Informe um UF válido"));
+	    if ((uf == null || uf.isBlank()) && (municipioNome == null || municipioNome.isBlank()))
+	        return ResponseEntity.badRequest().body("Informe pelo menos um UF ou Nome do Município");
+	    
+	    if (uf != null && !uf.isBlank()) {
+	        if (uf.trim().isEmpty() || uf.length() != 2 || uf.replaceAll("[\"']", "").trim().isEmpty())
+	            return ResponseEntity.badRequest().body("Informe um UF válido");
+	        uf = uf.toUpperCase();
+	    }
+	    
+	    if (municipioNome != null && municipioNome.isBlank())
+	        return ResponseEntity.badRequest().body("Informe um Nome de Município válido");
 
-	    return ResponseEntity.ok(repository
-	    		.findByUfSigla(uf.toUpperCase())
-	    		.stream()
-	    		.map(municipio -> new MunicipioDTO(municipio))
-	    	    .toList());
+	    List<Municipio> municipios;
+	    if (uf != null)
+	        municipios = repository.findByUfSigla(uf);
+	    else
+	        municipios = repository.findByNome("%" + municipioNome + "%");
+	    
+	    return ResponseEntity.ok(
+	        municipios.stream()
+	            .map(municipio -> new MunicipioDTO(municipio))
+	            .collect(Collectors.toList()));
 	}
 }
--- src/main/java/com/github/devmribeiro/api/municipios/repository/MunicipioRepository.java
+++ src/main/java/com/github/devmribeiro/api/municipios/repository/MunicipioRepository.java
@@ -11,11 +11,11 @@ import com.github.devmribeiro.api.municipios.model.Municipio;
 
 @Repository
 public interface MunicipioRepository extends JpaRepository<Municipio, Long> {
-	Municipio findByNome(String nome);
 
-	@Query(value = "SELECT m.* FROM municipio m JOIN FETCH unidade_federativa uf ON m.mun_uf_cod = uf.uf_cod WHERE uf.uf_sigla = :sigla", nativeQuery = true)
-	List<Municipio> findByUfSigla(@Param("sigla") String sigla);
-	
-	@Query("SELECT m FROM Municipio m JOIN FETCH m.uf")
-	List<Municipio> findAll();
+    @Query(value = "SELECT m.* FROM municipio m WHERE m.mun_nome ILIKE :nome ESCAPE '\\'", nativeQuery = true)
+    List<Municipio> findByNome(@Param("nome") String nome);
+
+    // Ajustada a consulta para SQL nativo
+    @Query(value = "SELECT m.* FROM municipio m JOIN unidade_federativa uf ON m.mun_uf_cod = uf.uf_cod WHERE uf.uf_sigla = :sigla", nativeQuery = true)
+    List<Municipio> findByUfSigla(@Param("sigla") String sigla);
 }
